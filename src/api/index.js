3affc61404f56995dc85eb4a09003f7de662156e
Doondoony <cream.doondoon@gmail.com>
2018-10-09 13:31:01 +0900

    Initialize Project

--- src/api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router({});
-const todo = require('./todo');
-
-router.use('/todo', todo);
-
-module.exports = router;
--- src/api/todo.js
+++
@@ -1,9 +0,0 @@
-const todoController = require('./todo.ctrl');
-const todoRouter = require('express').Router({});
-
-todoRouter.get('/', todoController.retrieveTodo);
-todoRouter.post('/', todoController.createTodo);
-todoRouter.put('/', todoController.updateTodo);
-todoRouter.delete('/', todoController.deleteTodo);
-
-module.exports = todoRouter;
